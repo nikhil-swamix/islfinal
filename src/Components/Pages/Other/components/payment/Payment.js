cb546f68c318c6d21a4afdd096a5aede7ae84ef9
MD Ayaz <iamayazhussain@gmail.com>
2023-07-11 13:53:16 -0500

    071123

--- src/Components/Pages/Other/components/payment/Payment.js
+++ src/Components/Pages/Other/components/payment/Payment.js
@@ -50,7 +50,7 @@ const Payment = () => {
                         <div class="courses_sidebar_list">
                             <ul class="list">
                                 <li> Last Date: 25-07-2023</li>
-                                <li><a href="https://drive.google.com/file/d/15kRu5fi0k_W120LYjJG8egg_6Y16b1Lx/view?usp=sharing"><i class="fa fa-eye"></i> View Notification </a></li>
+                                <li><a href="https://drive.google.com/file/d/1sW7pKF9E0wrnCyQw2Aeq9hSbxFkcy0EU/view?usp=sharing"><i class="fa fa-eye"></i> View Notification </a></li>
                             </ul>
                             <ul class="list">
                                 <li> View Payment History</li>
